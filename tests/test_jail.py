38e020cb78b1f2ef6840cfeb97adb067f9dd738d
Stanislaw Adaszewski <s.adaszewski@gmail.com>
2020-06-28 06:43:54 +0200

    Add test_jail_run_01().

--- tests/test_jail.py
+++ tests/test_jail.py
@@ -3,7 +3,8 @@ from focker.jail import backup_file, \
     jail_create, \
     get_jid, \
     do_mounts, \
-    undo_mounts
+    undo_mounts, \
+    jail_run
 from focker.jailspec import gen_env_command, \
     quote
 import tempfile
@@ -16,6 +17,14 @@ from focker.zfs import zfs_mountpoint, \
 import jailconf
 import shutil
 from focker.mount import getmntinfo
+
+
+def setup_module(module):
+    subprocess.check_output(['focker', 'bootstrap', '--non-interactive', '--tags', 'test-jail'])
+
+
+def teardown_module(module):
+    subprocess.check_output(['focker', 'image', 'remove', 'test-jail'])
 
 
 def test_backup_file():
@@ -130,26 +139,12 @@ def test_jail_create():
 
 
 def test_get_jid_01():
-    os.dup(1)
-    with open('/dev/null', 'a') as devnull:
-        os.dup2(devnull.fileno(), 3)
-        subprocess.check_output(['focker', 'bootstrap', '--non-interactive', '--tags', 'test-get-jid-01'], stderr=subprocess.PIPE)
-    # os.makedirs(os.path.join(mountpoint, 'usr/bin'))
-    # os.makedirs(os.path.join(mountpoint, 'lib'))
-    # os.makedirs(os.path.join(mountpoint, 'dev'))
-    # os.makedirs(os.path.join(mountpoint, 'etc'))
-    # shutil.copyfile('/etc/master.passwd', os.path.join(mountpoint, 'etc/master.passwd'))
-    # shutil.copyfile('/usr/bin/true', os.path.join(mountpoint, 'usr/bin/true'))
-    # shutil.copyfile('/lib/libc.so.7', os.path.join(mountpoint, 'lib/libc.so.7'))
-    # subprocess.check_output(['zfs', 'set', 'rdonly=on', name])
-    # subprocess.check_output(['zfs', 'snapshot', name + '@1'])
-    subprocess.check_output(['focker', 'jail', 'create', 'test-get-jid-01', '--tags', 'test-get-jid-01', '--command', '/usr/bin/true'])
+    subprocess.check_output(['focker', 'jail', 'create', 'test-jail', '--tags', 'test-get-jid-01', '--command', '/usr/bin/true'])
     name, _ = zfs_find('test-get-jid-01', focker_type='jail')
     mountpoint = zfs_mountpoint(name)
     subprocess.check_output(['focker', 'jail', 'start', 'test-get-jid-01'])
     _ = get_jid(mountpoint)
     subprocess.check_output(['focker', 'jail', 'remove', 'test-get-jid-01'])
-    subprocess.check_output(['focker', 'image', 'remove', 'test-get-jid-01'])
 
 
 def test_do_mounts_01():
@@ -180,3 +175,14 @@ def test_undo_mounts_01():
             lst = [ a for a in getmntinfo() \
                 if a['f_mntfromname'] == d2.encode('utf-8') ]
             assert len(lst) == 0
+
+
+def test_jail_run_01():
+    subprocess.check_output(['focker', 'jail', 'create', 'test-jail', '--tags', 'test-jail-run-01'])
+    name, _ = zfs_find('test-jail-run-01', focker_type='jail')
+    mountpoint = zfs_mountpoint(name)
+    jail_run(mountpoint, 'echo test-jail-run-01 >/tmp/test.txt')
+    assert os.path.exists(os.path.join(mountpoint, 'tmp/test.txt'))
+    with open(os.path.join(mountpoint, 'tmp/test.txt'), 'r') as f:
+        assert f.read() == 'test-jail-run-01\n'
+    subprocess.check_output(['focker', 'jail', 'remove', 'test-jail-run-01'])
