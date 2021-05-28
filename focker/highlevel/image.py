212429b023303548261f18158a06b576bc6aedb2
Stanislaw Adaszewski <s.adaszewski@gmail.com>
2021-05-28 11:39:39 +0200

    Small fix

--- focker/highlevel/image.py
+++ focker/highlevel/image.py
@@ -7,18 +7,19 @@ from ..zfs2 import zfs_shortest_unique_name, \
 from ..snapshot import new_snapshot
 from ..misc import find_prefix
 
+Image='Image'
 
 class Image:
     __init_key = object()
 
-    def __init__(self, init_key, name, sha256, mountpoint, finalized):
+    def __init__(self, init_key, name, sha256, mountpoint, is_finalized):
         if not init_key == self.__init_key:
             raise RuntimeError('Image must be created using one of the factory methods')
 
         self.name = name
         self.sha256 = sha256
         self.mountpoint = mountpoint
-        self.finalized = finalized
+        self.is_finalized = is_finalized
 
     @staticmethod
     def handle_from_predicate_corner_cases(lst):
@@ -32,11 +33,12 @@ class Image:
         lst = zfs_list(['name', 'mountpoint', 'focker:sha256', 'focker:tags', 'rdonly'],
             focker_type='image', zfs_type='filesystem')
         lst = [ e for e in lst if pred(e) ]
+        # print(lst)
         Image.handle_from_predicate_corner_cases(lst)
-        name, mountpoint, sha256, _, rdonly = lst[0]
-        finalized = (rdonly == 'on')
+        name, mountpoint, sha256, _, rdonly, *_ = lst[0]
+        is_finalized = (rdonly == 'on')
         return Image(Image.__init_key, name=name, sha256=sha256,
-            mountpoint=mountpoint, finalized=finalized)
+            mountpoint=mountpoint, is_finalized=is_finalized)
 
     @staticmethod
     def from_sha256(sha256: str):
@@ -58,11 +60,13 @@ class Image:
 
     @staticmethod
     def from_base(base: Image, sha256: str) -> Image:
+        if not base.is_finalized:
+            raise RuntimeError('Base must be finalized')
         name = zfs_shortest_unique_name(sha256, focker_type='image')
         zfs_clone(base.snapshot_name(), name, { 'focker:sha256': sha256 })
         mountpoint = zfs_mountpoint(name)
         return Image(Image.__init_key, name=name, sha256=sha256,
-            mountpoint=mountpoint, finalized=False)
+            mountpoint=mountpoint, is_finalized=False)
 
     def apply_spec(self):
         raise NotImplementedError
@@ -70,7 +74,7 @@ class Image:
     def finalize(self):
         zfs_set_props(self.name, { 'rdonly': 'on' })
         zfs_snapshot(self.snapshot_name())
-        self.finalized = True
+        self.is_finalized = True
 
     def create(self):
         raise NotImplementedError
