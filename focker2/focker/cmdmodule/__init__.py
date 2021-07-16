5c57d3d8636473796fdff4f74f7ee0dcfad3c727
Stanislaw Adaszewski <s.adaszewski@gmail.com>
2021-07-16 12:10:41 +0200

    Start ComposePlugin.

--- focker2/focker/cmdmodule/__init__.py
+++ focker2/focker/cmdmodule/__init__.py
@@ -2,3 +2,4 @@ from .image import ImagePlugin
 from .bootstrap import BootstrapPlugin
 from .volume import VolumePlugin
 from .jail import JailPlugin
+from .compose import ComposePlugin
---
+++ focker2/focker/cmdmodule/compose.py
@@ -0,0 +1,26 @@
+from ..plugin import Plugin
+from ..core import Volume
+
+
+class ComposePlugin(Plugin):
+    @staticmethod
+    def provide_parsers():
+        return dict(
+            compose=dict(
+                aliases=['comp', 'cmp', 'c'],
+                subparsers=dict(
+                    build=dict(
+                        aliases=['bld', 'b'],
+                        func=cmd_compose_build,
+                        spec_filename=dict(
+                            positional=True,
+                            type=str
+                        )
+                    )
+                )
+            )
+        )
+
+
+def cmd_compose_build(args):
+    raise NotImplementedError
