f48ec88887ecd2137fe2ae6d846e52d4136199b6
Stanislaw Adaszewski <s.adaszewski@gmail.com>
2021-07-23 10:23:20 +0200

    Add ImageBuildJailSpec and OneExecJailSpec.

--- focker2/focker/core/jailspec/variants.py
+++ focker2/focker/core/jailspec/variants.py
@@ -16,3 +16,30 @@ class CloneImageJailSpec(JailSpec):
         jailspec['path'] = jfs.path
         jailspec['name'] = os.path.split(jfs.path)[-1]
         return cls._from_dict(jailspec)
+
+
+class ImageBuildJailSpec(JailSpec):
+    @classmethod
+    def from_image_and_dict(cls, im: Image, jailspec: Dict):
+        if 'image' in jailspec:
+            raise RuntimeError('image should be specified separately')
+        jailspec = dict(jailspec)
+        jailspec['path'] = im.path
+        jailspec['name'] = os.path.split(im.path)[-1]
+        return cls._from_dict(jailspec)
+
+del ImageBuildJailSpec.from_dict
+
+
+class OneExecJailSpec(JailSpec):
+    @classmethod
+    def from_image_and_dict(cls, im: Image, jailspec: Dict):
+        if 'image' in jailspec:
+            raise RuntimeError('image should be specified separately')
+        jfs = JailFs.clone_from(im)
+        jailspec = dict(jailspec)
+        jailspec['path'] = jfs.path
+        jailspec['name'] = os.path.split(jfs.path)[-1]
+        return cls._from_dict(jailspec)
+
+del OneExecJailSpec.from_dict
