4252bc05a95a96e35f42c028f79b737ff65e1846
Stanislaw Adaszewski <s.adaszewski@gmail.com>
2021-07-24 17:10:46 +0200

    Add overrides.py.

---
+++ focker2/focker/misc/overrides.py
@@ -0,0 +1,29 @@
+import os
+import ruamel.yaml as yaml
+
+
+def load_overrides(fname, env_prefix='FOCKER_', env_hier=False):
+    res = {}
+    for p in [ os.path.expanduser('~/.focker'), '/usr/local/etc/focker',
+        '/etc/focker' ]:
+        p = os.path.join(p, fname)
+        if not os.path.exists(p):
+            continue
+        with open(p) as f:
+            res = yaml.safe_load(f)
+        break
+    for k, v in os.environ.items():
+        if not k.startswith(env_prefix):
+            continue
+        k = k[len(env_prefix):].lower()
+        if env_hier:
+            k = k.split('_')
+            r = res
+            for p in k[:-1]:
+                if not p in r:
+                    r[p] = {}
+                r = r[p]
+            r[k[-1]] = v
+        else:
+            r[k] = v
+    return res
