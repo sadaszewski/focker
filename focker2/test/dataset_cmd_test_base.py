a14fa2d0b5a037f8d53f1d95e7059d08a00b9ef1
Stanislaw Adaszewski <s.adaszewski@gmail.com>
2021-08-03 14:58:34 +0200

    More tests.

--- focker2/test/dataset_cmd_test_base.py
+++ focker2/test/dataset_cmd_test_base.py
@@ -10,15 +10,33 @@ import re
 class DatasetCmdTestBase:
     _meta_class = None
 
-    def test00_list_simple(self):
+    def _create(self):
+        cmd = [ self._meta_class._meta_focker_type, 'create' ]
+        buf = StringIO()
+        with redirect_stdout(buf):
+            main(cmd)
+        m = re.findall('Created ([^ ]+) mounted at ([^ ]+)', buf.getvalue())
+        assert len(m) == 1
+        name = m[0][0]
+        return name
+
+    def test01_list_simple(self):
         cmd = [ self._meta_class._meta_focker_type, 'list' ]
         main(cmd)
 
+    def test02_list_sort(self):
+        cmd = [ self._meta_class._meta_focker_type, 'list', '-s', 'sha256' ]
+        main(cmd)
+
+    def test03_list_output(self):
+        cmd = [ self._meta_class._meta_focker_type, 'list', '-o', 'is_protected' ]
+        main(cmd)
+
     @pytest.mark.skip
-    def test01_list_before_after_create(self):
+    def test04_list_before_after_create(self):
         pass
 
-    def test02_create(self):
+    def test05_create(self):
         cmd = [ self._meta_class._meta_focker_type, 'create' ]
         buf = StringIO()
         with redirect_stdout(buf):
@@ -29,13 +47,29 @@ class DatasetCmdTestBase:
         assert zfs_exists(name)
         zfs_destroy(name)
 
-    @pytest.mark.skip
-    def test03_prune(self):
-        pass
+    def test06_prune(self):
+        cmd = [ self._meta_class._meta_focker_type, 'create' ]
+        buf = StringIO()
+        with redirect_stdout(buf):
+            main(cmd)
+        m = re.findall('Created ([^ ]+) mounted at ([^ ]+)', buf.getvalue())
+        assert len(m) == 1
+        name = m[0][0]
+        assert zfs_exists(name)
+        cmd = [ self._meta_class._meta_focker_type, 'prune' ]
+        main(cmd)
+        assert not zfs_exists(name)
 
-    @pytest.mark.skip
-    def test04_tag(self):
-        pass
+    def test07_tag(self):
+        name = self._create()
+        assert zfs_exists(name)
+        ds = self._meta_class.from_name(name)
+        try:
+            cmd = [ self._meta_class._meta_focker_type, 'tag', ds.sha256, 'focker-unit-test-tag' ]
+            main(cmd)
+            assert 'focker-unit-test-tag' in ds.tags
+        finally:
+            ds.destroy()
 
     @pytest.mark.skip
     def test05_untag(self):
