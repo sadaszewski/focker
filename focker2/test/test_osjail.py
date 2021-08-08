0a22cbbd42f49e039e954bc87efba430d4a457be
Stanislaw Adaszewski <s.adaszewski@gmail.com>
2021-08-08 14:57:37 +0200

    osjail full coverage.

--- focker2/test/test_osjail.py
+++ focker2/test/test_osjail.py
@@ -3,6 +3,10 @@ from focker.core import OSJail, \
     JailFs, \
     CloneImageJailSpec
 from focker.core.jailspec import JailSpec
+import pytest
+import os
+import focker.core.osjail as osjail
+import json
 
 
 class TestOSJail:
@@ -57,3 +61,123 @@ class TestOSJail:
             jail.stop()
         finally:
             spec.jfs.destroy()
+
+    def test05_factory_methods(self):
+        with pytest.raises(RuntimeError, match='factory methods'):
+            _ = OSJail()
+
+    def test06_name_not_found(self):
+        with pytest.raises(RuntimeError, match='not found'):
+            _ = OSJail.from_name('focker-unit-test-impossible-jail-name')
+
+    def test07_mountpoint_not_found(self):
+        with pytest.raises(RuntimeError, match='not found'):
+            _ = OSJail.from_mountpoint('focker-unit-test-impossible-jail-mountpoint')
+
+    def test08_tag_not_found_noraise(self):
+        res = OSJail.from_tag('focker-unit-test-impossible-jail-tag', raise_exc=False)
+        assert res is None
+
+    def test09_anyid_not_found_noraise(self):
+        res = OSJail.from_any_id('focker-unit-test-impossible-identifier', raise_exc=False)
+        assert res is None
+
+    def test10_exec_fib(self):
+        spec = CloneImageJailSpec.from_dict({ 'image': 'freebsd-latest', 'exec.fib': 0 })
+        try:
+            ospec = OSJailSpec.from_jailspec(spec)
+            jail = ospec.add()
+            jail.start()
+            cmd = None
+            def dummywrapper(cmd_in, *args, **kwargs):
+                nonlocal cmd
+                cmd = cmd_in
+            jail.jexec(['ls', '-al'], dummywrapper)
+            assert 'setfib' in cmd
+        finally:
+            spec.jfs.destroy()
+
+    def test11_mountpoint_not_found_noraise(self):
+        res = OSJail.from_mountpoint('focker-unit-test-impossible-jail-mountpoint', raise_exc=False)
+        assert res is None
+
+    def test12_from_tag(self):
+        jail, jfs = self._create()
+        try:
+            jfs.add_tags([ 'focker-unit-test-jail-from-tag' ])
+            jail_1 = OSJail.from_tag('focker-unit-test-jail-from-tag')
+            assert jail.name == jail_1.name
+        finally:
+            jfs.destroy()
+
+    def test13_run(self):
+        spec = CloneImageJailSpec.from_dict({ 'image': 'freebsd-latest' })
+        try:
+            ospec = OSJailSpec.from_jailspec(spec)
+            jail = ospec.add()
+            jail.start()
+            jail.run([ 'touch', '/.focker-unit-test-jail-run' ])
+            assert os.path.exists(os.path.join(spec.jfs.path, '.focker-unit-test-jail-run'))
+        finally:
+            spec.jfs.destroy()
+
+    def test14_jls_not_running(self):
+        jail, jfs = self._create()
+        try:
+            with pytest.raises(RuntimeError, match='Not running'):
+                _ = jail.jls()
+        finally:
+            jfs.destroy()
+
+    def test15_multiple_jails_same_name(self, monkeypatch):
+        def mock(cmd, *args, **kwargs):
+            res = json.dumps({
+                'jail-information': dict(
+                    jail=[
+                        dict(name='focker_focker-unit-test-jail'),
+                        dict(name='focker_focker-unit-test-jail')
+                    ])
+            })
+            # print('Mock was called with cmd:', cmd, 'returning:', res)
+            return res
+        jail, jfs = self._create()
+        # print('jail.name:', jail.name)
+        try:
+            with monkeypatch.context() as m:
+                m.setattr(osjail, 'focker_subprocess_check_output', mock)
+                with pytest.raises(RuntimeError, match='Multiple jails'):
+                    _ = jail.jls()
+        finally:
+            jfs.destroy()
+
+    def test16_runtime_property(self):
+        spec = CloneImageJailSpec.from_dict({ 'image': 'freebsd-latest' })
+        try:
+            ospec = OSJailSpec.from_jailspec(spec)
+            jail = ospec.add()
+            jail.start()
+            assert jail.has_runtime_property('persist')
+            assert jail.get_runtime_property('persist') == True
+        finally:
+            spec.jfs.destroy()
+
+    def test17_jid(self):
+        spec = CloneImageJailSpec.from_dict({ 'image': 'freebsd-latest' })
+        try:
+            ospec = OSJailSpec.from_jailspec(spec)
+            jail = ospec.add()
+            assert jail.jid is None
+            jail.start()
+            assert jail.jid is not None
+            jail.stop()
+            assert jail.jid is None
+        finally:
+            spec.jfs.destroy()
+
+    def test18_conf(self):
+        jail, jfs = self._create()
+        try:
+            conf = jail.conf
+            assert conf['persist'] == True
+        finally:
+            jfs.destroy()
