74441272eb00ad044545d9ded5c2170118b81674
Stanislaw Adaszewski <s.adaszewski@adared.ch>
2021-12-15 20:25:10 +0100

    Add OSJail.from_sha256.

--- focker/core/osjail/osjail.py
+++ focker/core/osjail/osjail.py
@@ -70,9 +70,7 @@ class OSJail:
     def from_sha256(cls, sha256):
         from ..jailfs import JailFs
         jfs = JailFs.from_sha256(sha256)
-        if jfs is not None:
-            return cls.from_mountpoint(jfs.mountpoint)
-        return None
+        return cls.from_mountpoint(jfs.mountpoint)
 
     def start(self):
         focker_subprocess_run([ 'jail', '-c', self.name ])
