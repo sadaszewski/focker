601a617714d7727c0b3d0e2c4566654e28ad48f3
Stanislaw Adaszewski <s.adaszewski@adared.ch>
2022-11-29 22:46:12 +0100

    Optimized prune for images.

--- focker/core/image/image.py
+++ focker/core/image/image.py
@@ -7,7 +7,9 @@
 
 
 from ..dataset import Dataset
-from ..zfs import zfs_list
+from ..zfs import zfs_list, \
+    zfs_destroy
+from functools import reduce
 
 
 Image='Image'
@@ -29,6 +31,38 @@ class Image(Dataset):
         lst = zfs_list(fields, focker_type='image')
         lst = [ Image.from_name(item[0]) for item in lst if item[0] not in used ]
         return lst
+
+    @staticmethod
+    def prune():
+        fields = [ 'name', 'origin', 'focker:tags' ]
+        tree = {}
+        for focker_type in [ 'image', 'jail' ]:
+            lst = zfs_list(fields, focker_type=focker_type)
+            for name, origin, tags, *_ in lst:
+                origin = origin.split('@')[0]
+                node = tree.get(origin, {})
+                children = node.get('children', [])
+                children.append(name)
+                node['children'] = children
+                tree[origin] = node
+                if focker_type == 'image' and tags == '-':
+                    node = tree.get(name, {})
+                    node.update({ 'parent': origin, 'tags': tags })
+                    tree[name] = node
+        batches = []
+        while True:
+            bat = set([ k for k, v in tree.items() if not v.get('children') ])
+            if not bat:
+                break
+            batches.append(bat)
+            tree = { k: v for k, v in tree.items() if k not in bat }
+            for name in bat:
+                for v in tree.values():
+                    if name in v.get('children', []):
+                        v['children'].remove(name)
+        batches = reduce(list.__add__, map(list, batches), [])
+        for name in batches:
+            zfs_destroy(name)
 
 Image._meta_class = Image
 Image._meta_cloneable_from = Image
