fdcae1729bad17746f9feb18b385a77cc83297bc
Stanislaw Adaszewski <s.adaszewski@adared.ch>
2022-12-03 16:14:09 +0100

    focker_service works, the problem was with an image.

--- scripts/migrate_jailconf.py
+++ scripts/migrate_jailconf.py
@@ -1,10 +1,14 @@
 from focker.jailconf import load
 from focker.misc.load_jailconf import *
+from focker.core import FOCKER_CONFIG
 
 
 def main():
     jc = load()
     for name, entry in jc.jail_blocks.items():
+        if not entry.safe_get('path', '').startswith(FOCKER_CONFIG.zfs.root_mountpoint + '/jails/'):
+            print(f'Skipping non-Focker jail: {name}')
+            continue
         print(name)
         entry = entry.to_dict()
         jailconf_add_jail(name=name, entry=entry)
